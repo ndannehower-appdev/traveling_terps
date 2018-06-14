631a43d7f31f939782eb90a5790831097cb2606a
Raghu Betina <raghu@firstdraft.com>
2018-06-14 15:48:42 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "recommendations#index"
   # Routes for the Rating resource:
   # CREATE
   get "/ratings/new", :controller => "ratings", :action => "new"

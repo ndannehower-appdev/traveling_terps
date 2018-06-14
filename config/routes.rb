bfed210d5d3b8399f667df7361399845b05e1670
Raghu Betina <raghu@firstdraft.com>
2018-06-14 15:48:37 +0000

    Generate trips resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Trip resource:
+  # CREATE
+  get "/trips/new", :controller => "trips", :action => "new"
+  post "/create_trip", :controller => "trips", :action => "create"
+
+  # READ
+  get "/trips", :controller => "trips", :action => "index"
+  get "/trips/:id", :controller => "trips", :action => "show"
+
+  # UPDATE
+  get "/trips/:id/edit", :controller => "trips", :action => "edit"
+  post "/update_trip/:id", :controller => "trips", :action => "update"
+
+  # DELETE
+  get "/delete_trip/:id", :controller => "trips", :action => "destroy"
+  #------------------------------
+
   # Routes for the Year resource:
   # CREATE
   get "/years/new", :controller => "years", :action => "new"

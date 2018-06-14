cf05d74234d366cf15f18b9fa75df8668884c839
Raghu Betina <raghu@firstdraft.com>
2018-06-14 15:51:14 +0000

    Whitelist columns in Month for ActiveAdmin

--- app/admin/months.rb
+++ app/admin/months.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Month do
+
+ permit_params :month
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
